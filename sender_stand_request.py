9e7b1aff641f9350bffe0cc430ddce54b8db5b66
TatianaEvK <134235181+TatianaEvK@users.noreply.github.com>
2023-05-28 16:13:37 +0300

    Update sender_stand_request.py

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -4,26 +4,22 @@ import data
 
 #Запрос на создание заказа.
 def post_new_order(order_body):
-    order_body = data.order_body
     return requests.post(configuration.URL_SERVICE + configuration.CREATE_ORDERS_PATH,
                          json=order_body)
 
 # Сохранение номера трека заказа.
 def get_new_track():
-    order_body = data.order_body.copy()
-    res = post_new_order(order_body)
-    t = res.json()["track"]
-    return t
+    res = post_new_order(data.order_body)
+    track = res.json()["track"]
+    return track
 
 
 
 # Запрос на получения заказа по треку.
 
-def get_order_body():
-    current_order_body = data.order_body.copy()
-    current_order_body["t"] = 't'
-    return  current_order_body
-
+def get_order_body(track):
+    params_with_track= {"t": track} 
+    return requests.get(configuration.URL_SERVICE + configuration.RECEIVE_ORDER, params=params_with_track) 
 
 
 
